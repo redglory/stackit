fe134e535c22063ddc5c7c06c0f639734d4da998
redglory <pedropinto.inbox@gmail.com>
2014-06-30 15:20:06 +0100

    added ffmpeg and single folder stack option

--- stackit.py
+++ stackit.py
@@ -36,21 +36,35 @@ def getMovieFiles(argv):
     exts.append(".CD{0}.avi".format(x))
     exts.append(".CD{0}.mkv".format(x))
     exts.append(".CD{0}.mp4".format(x))
-	
-  print("Please wait, searching for unstacked movies on library: %s..."% os.path.normpath(argv[0]).upper())
-  Logger.info("Searching for unstacked movies on library: %s..."% os.path.normpath(argv[0]).upper())	
   
-  for root, dirs, files in os.walk(argv[0]):			
+  print("Please wait, searching for unstacked movies on library: %s..."% os.path.normpath(argv[0]).upper())
+  Logger.info("Searching for unstacked movies on library: %s..."% os.path.normpath(argv[0]).upper())
+
+  print "Searching movie files on root directory..."
+  Logger.info("Searching files on root directory...")
+  for f in os.listdir(argv[0]):
+    if os.path.isfile(os.path.join(argv[0], f)):
+      for ext in exts:
+        uppername = f.upper()
+        if uppername.find(ext.upper()) != -1:
+          fname = os.path.join(argv[0], f)
+          dirfiles.append(fname)
+          Logger.debug("Added movie part file %s ... !", fname)
+      filelist[argv[0]] = dirfiles
+  dirfiles=[]
+  
+  for root, dirs, files in os.walk(argv[0]):
     for dir in dirs:    
+      print ("Searching movie files on %s sub-directory..."% dir)
       for ext in exts:
         for name in os.listdir(os.path.join(root, dir)):
           uppername = name.upper()
           if uppername.find(ext.upper()) != -1:
-            dirfiles.append(os.path.join(root, dir, name))
+            fname = os.path.join(root, dir, name)
+            dirfiles.append(fname)
             filelist[os.path.join(root, dir)] = dirfiles
       dirfiles=[]
 
-  #movielist = sorted(filelist.items(), key = lambda t: t[0])
   return collections.OrderedDict(sorted(filelist.items(), key = lambda t: t[0]))
  
 def remove_file(filepath):
@@ -83,14 +97,13 @@ def process(movies):
     try:
       print("Please wait, stacking movie: %s..."% moviepath)
       Logger.info("Stacking movie: %s"% moviepath)
-      ffmpeg_command = ["ffmpeg", "-f", "concat", "-i", movietxt, "-c", "copy", "-y", movie_output]
+      ffmpeg_command = ["bin\\ffmpeg.exe", "-f", "concat", "-i", movietxt, "-c", "copy", "-y", movie_output]
       Logger.info("Running FFMPEG with arguments: %s"% ffmpeg_command)
       #response = subprocess.check_output(ffmpeg_command, stderr=subprocess.STDOUT).decode("utf-8")
       ffmpeg = subprocess.Popen(ffmpeg_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
       output, errors = ffmpeg.communicate()
       if ffmpeg.returncode == 0:	  
         print("Stacking movie: %s finished successfully!"% moviepath)
-        #Logger.info(output + '\n')
         Logger.info("Stacking movie: %s finished successfully!"% moviepath)
         time.sleep(2)
         Logger.info("Cleaning Up movie: %s by removing part files and temporary input file: %s!"% (moviepath, movietxt))
@@ -122,11 +135,11 @@ def process(movies):
   print(" [%i] Movie(s) generated processing errors! "% movie_error_count)
   print("#############################################")
   Logger.info("##############################################")
-  Logger.info("				Runtime: %s seconds				     "% proc_time)
+  Logger.info("				Runtime: %s seconds		         "% proc_time)
   Logger.info("##############################################")
-  Logger.info(" [%i] Movie(s) were stacked successfully.						"% movie_count)
+  Logger.info(" [%i] Movie(s) were stacked successfully.     "% movie_count)
   Logger.info("##############################################")
-  Logger.info(" [%i] Movie(s) generated processing errors!					    "% movie_error_count)
+  Logger.info(" [%i] Movie(s) generated processing errors!   "% movie_error_count)
   Logger.info("##############################################")
   
 def main(argv):
@@ -139,17 +152,15 @@ def main(argv):
   
   movielist = getMovieFiles(argv)
   if not movielist:
-    print("No movies on %s to process" % argv[0])
+    print("No movies on %s to process"% os.path.normpath(argv[0]).upper())
+    Logger.info("No movies on %s to process"% os.path.normpath(argv[0]).upper())
   else:
     process(movielist)
 
 if __name__ == "__main__":
 
   try:
-    if sys.platform == "win32":
-      Logger = create_log(os.path.join(os.getcwd(), "logs\stackit.log"))
-    else:
-      Logger = create_log(os.path.join(os.getcwd(), "logs/stackit.log"))
+    Logger = create_log(os.path.join(os.getcwd(), 'logs' + os.sep + 'stackit.log'))
     main(sys.argv[1:])
   except (KeyboardInterrupt, SystemExit) as e:
     if type(e) == SystemExit: sys.exit(int(str(e)))
